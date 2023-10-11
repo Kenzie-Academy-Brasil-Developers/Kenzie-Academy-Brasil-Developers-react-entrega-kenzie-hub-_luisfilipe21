c5fd17db2a600db43d90c6d07f2979145bbbb84d
Luis Filipe Guimarães Costa <luisfilipegc@hotmail.com>
2023-10-11 19:33:25 -0300

    feat: criação da estrutura base do projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
+import { Routes } from "react-router-dom"
+import { DefaultTemplate } from "./components/DefaultTemplate"
 
 function App() {
 
   return (
-    <>
-      
-      
-    </>
+    <DefaultTemplate>
+      <Routes />
+    </DefaultTemplate>
   )
 }
 
---
+++ src/routes/index.jsx
@@ -0,0 +1,12 @@
+import { Route, Routes, useNavigate } from 'react-router-dom';
+
+export const Routes = () => {
+    
+    return (
+        <Routes>
+            <Route />
+            <Route />
+            <Route />
+        </Routes>
+    )
+}
